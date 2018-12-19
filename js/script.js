b4a37919db8ff03ecbf54aa345cb7f240cba0d34
Serhat Kaya <kaya.serrhat@gmail.com>
2018-12-19 17:51:18 +0300

    Finished version 1.0

--- js/script.js
+++ js/script.js
@@ -40,10 +40,31 @@ function createGrid(gridSize) {
         item.style.width = (sizeParam-1)+'px';
         item.style.height = (sizeParam-1)+'px';
         item.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'black';
+            if(e.target.style.backgroundColor === '') {
+                var r = 1 + Math.floor(Math.random()*255);
+                var g = 1 + Math.floor(Math.random()*255);
+                var b = 1 + Math.floor(Math.random()*255);
+                e.target.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+            } else {
+                var color = e.target.style.backgroundColor;
+                e.target.style.backgroundColor = blackFadingRGB(color);
+            }
         });
     });
 }
 
+function blackFadingRGB(color) {
+    var colorSliced = color.split(',')
+    var r = parseInt(colorSliced[0].slice(4));
+    var g = parseInt(colorSliced[1].trim());
+    var b = parseInt(colorSliced[2].slice(0, -1));
+    if(r > 50) r = r - 50;
+    else r = 0;
+    if(g > 50) g = g - 50;
+    else g = 0;
+    if(b > 50) b = b - 50;
+    else b = 0;
 
+    return 'rgb('+r+','+g+','+b+')'
+}
 
